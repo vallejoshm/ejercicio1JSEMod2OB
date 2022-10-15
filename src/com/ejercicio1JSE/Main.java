3e2cf64ca172df97c641143cf66e60cdb3274db6
Hernan Vallejos <vallejoshm@gmail.com>
2022-10-15 09:28:40 -0300

    primer commit

---
+++ src/com/ejercicio1JSE/Main.java
@@ -0,0 +1,24 @@
+package com.ejercicio1JSE;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int varInt = -123;
+        long varLong = 155896895635563L;
+        double varDouble = -562547.336845D;
+        float varFloat = 12.3F;
+        boolean varBool = false;
+        boolean varBool2 =true;
+        String varString = "Hola mundos!";
+
+        System.out.println("Variable Int: " + varInt);
+        System.out.println("Variable Long: " + varLong);
+        System.out.println("Variable Double: " + varDouble);
+        System.out.println("Variable Float: " + varFloat);
+        System.out.println("Variable Boolean: " + varBool);
+        System.out.println("Variable Boolean: " + varBool2);
+        System.out.println("Variable String: " + varString);
+
+    }
+}
